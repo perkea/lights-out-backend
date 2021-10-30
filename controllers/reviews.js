59123d5d7a2a89f43fc49a5078ed3240e8a147d9
Keara Pereira <kearapereira@Kearas-MacBook-Pro.local>
2021-10-29 22:07:44 -0400

    review schema creaed

---
+++ controllers/reviews.js
@@ -0,0 +1,14 @@
+
+const express = require(`express`);
+const Movie = require("../models/movie");
+const Review = require("../models/review");
+const router = express.Router()
+
+
+//INDUCES
+
+// router.get("/", (req, res)=>{
+// res.send("Hello World");
+// })
+
+module.exports = router
