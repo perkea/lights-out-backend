022899821c7e965ab8547125e7ebce651305fdce
Keara Pereira <kearapereira@Kearas-MacBook-Pro.local>
2021-10-30 19:07:52 -0400

    seed data added

---
+++ seed.js
@@ -0,0 +1,20 @@
+const movieSeed = [
+{
+   
+    title:"Toy Story",
+    reviews: []
+}
+
+];
+
+
+const reviewSeed = [
+{
+    rating:3
+}
+
+
+]
+
+module.exports = movieSeed;
+module.exports = reviewSeed
