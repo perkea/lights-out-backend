1b37bd7b0a871a698b818b06a29f73d5174f10e8
Keara Pereira <kearapereira@Kearas-MacBook-Pro.local>
2021-10-30 19:10:27 -0400

    movie controller added

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,13 +1,13 @@
 const express = require(`express`);
 const Movie = require("../models/movie");
 const Review = require("../models/review");
-const router = express.Router()
+const movieRouter = express.Router()
 
 
 //INDUCES
+// ////////////////////////Seed route////////////////////////////////////
+ movieRouter.get("/", async(req, res)=>{
 
-// router.get("/", (req, res)=>{
-// res.send("Hello World");
-// })
+ });
 
-module.exports = router
+module.exports = movieRouter
