6a35ec9c7e79001423a78a3cff3c87990d13a3f6
Keara Pereira <kearapereira@Kearas-MacBook-Pro.local>
2021-10-29 22:09:59 -0400

    error corrected

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // =======================================
 //              DEPENDENCIES
 // =======================================
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express")
+const mongoose = require("mongoose")
 const logger = require('morgan');
 const cors = require("cors");
 const morgan = require("morgan");
