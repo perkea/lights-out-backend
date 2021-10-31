8f733577984bdde78618919786d853295ab5f5d2
Keara Pereira <kearapereira@Kearas-MacBook-Pro.local>
2021-10-30 23:32:12 -0400

    review schema modified

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,49 +1,48 @@
 const express = require(`express`);
 const Movie = require("../models/movie");
 const Review = require("../models/review");
-const reviewRouter = express.Router()
+const reviewRouter = express.Router();
+const seedData = require("../seedData");
 
+//TESTS
+reviewRouter.get("/addSeedData", (req, res) => {
+  Review.remove({}, () => {
+    seedData.forEach((element) => {
+      const review = new Review(element);
+      review.save();
+    });
+    res.status(200).json({ success: true, seedData: seedData });
+  });
+});
 
 //INDUCES
 // ////////////////////////Seed route////////////////////////////////////
-reviewRouter.get("/test", (req, res) => {
-    Review.find({
-        title: "Toy Story"
-    }).populate('movie').exec((error, foundReview) => {
-        res.send("done");
+reviewRouter.get("/", (req, res) => {
+  Review.find({}, (err, allReviews) => {
+    //   console.log("found allReview", allReviews)
+    res.status(200).json({ success: true, reviews: allReviews });
+  });
+});
+
+reviewRouter.get("/:id", (req, res) => {
+  Review.findById(req.params.id).exec((err, foundReview) => {
+    console.log(foundReview, err);
+    res.status(200).json({ success: true, review: foundReview });
+  });
+});
+
+reviewRouter.get("/moviesearch/:movieid", (req, res) => {
+  Review.find({ movieId: req.params.movieid }).exec((err, foundReviews) => {
+    console.log(foundReviews, err);
+    res.status(200).json({ success: true, review: foundReviews });
+  });
+});
+
+reviewRouter.post("/", (req, res) => {
+    const review = new Review(req.body);
+    review.save((error, savedReview) => {
+      res.status(200).json({ success: true, review: savedReview });
     });
-})
+  });
 
-// reviewRouter.get("/seed", (req, res) => {
-//     Review.deleteMany({}, (error, allReviews) => {
-//         Review.create(review, (error, data) => {
-//             res.redirect("/review");
-//         });
-//     });
-
-// });
-//////////////////////////Index/////////////////////////////////////////////////////////////////////////////
-// reviewRouter.get("/", isAuthenticated, (req, res) => {
-//     Recipe.find({}, (error, foundreview) => {
-//         res.render("review/index.ejs", {
-//             review: foundreview,
-//             tabTitle: 'Recipe Gallery',
-//         });
-//     });
-// });
-
-///////////////////////////////New///////////////////////////////////////////////////////////////////////
-// recipesRouter.get('/new', isAuthenticated, (req, res) => {
-//     res.render('recipes/new.ejs', {
-//         tabTitle: "Add a new Recipe",
-//     });
-// });
-// //////////////////////////// Delete////////////////////////////////////////////////////////////////////////////
-// recipesRouter.delete("/:id", isAuthenticated, (req, res) => {
-//     Recipe.findByIdAndRemove(req.params.id, (err, data) => {
-//         res.redirect("/recipes")
-//     })
-// })
-
-
-module.exports = reviewRouter
+module.exports = reviewRouter;
