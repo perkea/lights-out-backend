53acd2986dcb98e8ed8835842fa3290fba27ef83
Keara Pereira <kearapereira@Kearas-MacBook-Pro.local>
2021-10-30 23:33:59 -0400

    seed data added

---
+++ seedData.js
@@ -0,0 +1,46 @@
+// /const movieSchema = new Schema(
+//     {
+//       image: {
+//         type: String,
+//       },
+//       overview: {
+//         type: String,
+//       },
+//       rate: {
+//         type: Number,
+//         required: [true, "Please rate the movie"],
+//         min: [0, "Must be 0 or greater"],
+//         max: [5, "5 or less"],
+//       },
+//       review: {
+//         type: String,
+//       },
+
+
+// const seedData = [{
+//     overview: "this is the movie",
+//     rate: 5,
+//     review: "its a cool movie"}
+// ]
+
+// Reviews
+const seedData = [
+    {
+        rating: 5,
+        review: "Good movie",
+        movieId: 40561
+    },
+    {
+        rating: 1,
+        review: "Bad movie",
+        movieId: 63576
+    },
+    {
+        rating: 5,
+        review: "Very good!!!!!",
+        movieId: 63576
+
+    }
+];
+
+module.exports = seedData;
