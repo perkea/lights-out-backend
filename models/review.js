3d73993ddcd5fd056751ed65a4b80e33a2d765a0
Keara Pereira <kearapereira@Kearas-MacBook-Pro.local>
2021-10-30 21:09:20 -0400

    review schema created

--- models/review.js
+++ models/review.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const reviewSchema = new Schema(
-  {rating:Number
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+    },
+    movieId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Movie",
+    },
   },
   { timestamps: true }
 );
