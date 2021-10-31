5cf812b9b7c3aeb3eb6cbfc4f72e23088479ca6f
Keara Pereira <kearapereira@Kearas-MacBook-Pro.local>
2021-10-30 21:08:47 -0400

    movie sche,a created

--- models/movie.js
+++ models/movie.js
@@ -3,13 +3,21 @@ const Schema = mongoose.Schema;
 
 const movieSchema = new Schema(
   {
-    id: Number,
-    reviews: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Reviews",
-      },
-    ],
+    image: {
+      type: String,
+    },
+    overview: {
+      type: String,
+    },
+    rate: {
+      type: Number,
+      required: [true, "Please rate the movie"],
+      min: [0, "Must be 0 or greater"],
+      max: [5, "5 or less"],
+    },
+    review: {
+      type: String,
+    },
   },
   { timestamps: true }
 );
