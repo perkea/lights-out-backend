c33700f5b0471b8a52bf5d9b76e93d5913db5e3b
Keara Pereira <kearapereira@Kearas-MacBook-Pro.local>
2021-10-30 21:07:26 -0400

    trying to set up routes

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,14 +1,49 @@
-
 const express = require(`express`);
 const Movie = require("../models/movie");
 const Review = require("../models/review");
-const router = express.Router()
+const reviewRouter = express.Router()
 
 
 //INDUCES
+// ////////////////////////Seed route////////////////////////////////////
+reviewRouter.get("/test", (req, res) => {
+    Review.find({
+        title: "Toy Story"
+    }).populate('movie').exec((error, foundReview) => {
+        res.send("done");
+    });
+})
 
-// router.get("/", (req, res)=>{
-// res.send("Hello World");
+// reviewRouter.get("/seed", (req, res) => {
+//     Review.deleteMany({}, (error, allReviews) => {
+//         Review.create(review, (error, data) => {
+//             res.redirect("/review");
+//         });
+//     });
+
+// });
+//////////////////////////Index/////////////////////////////////////////////////////////////////////////////
+// reviewRouter.get("/", isAuthenticated, (req, res) => {
+//     Recipe.find({}, (error, foundreview) => {
+//         res.render("review/index.ejs", {
+//             review: foundreview,
+//             tabTitle: 'Recipe Gallery',
+//         });
+//     });
+// });
+
+///////////////////////////////New///////////////////////////////////////////////////////////////////////
+// recipesRouter.get('/new', isAuthenticated, (req, res) => {
+//     res.render('recipes/new.ejs', {
+//         tabTitle: "Add a new Recipe",
+//     });
+// });
+// //////////////////////////// Delete////////////////////////////////////////////////////////////////////////////
+// recipesRouter.delete("/:id", isAuthenticated, (req, res) => {
+//     Recipe.findByIdAndRemove(req.params.id, (err, data) => {
+//         res.redirect("/recipes")
+//     })
 // })
 
-module.exports = router
+
+module.exports = reviewRouter
