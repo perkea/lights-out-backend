1859719fba80be95fb3e8e5002068667a17db622
Keara Pereira <kearapereira@Kearas-MacBook-Pro.local>
2021-11-04 19:18:30 -0400

    seeding data

--- seedData.js
+++ seedData.js
@@ -16,6 +16,18 @@ const seedData = [
         comment :"Very good!!!!!",
         movieId: 63576
 
+    },
+    {
+        rating: 4,
+        comment :"Back to the Future is possibly my favourite trilogy series. And the first movie in that trilogy is not only one of the best time travel movies ever made but one of the best movies ever period.  ",
+        movieId: 105
+
+    },
+    {
+        rating: 5,
+        comment :"The 1980s and 70s  was the time where many classics were made ( godfather, back to the future, nightmare on elm street, jaws, etc.) However, this is my favorite of all of them. ",
+        movieId: 105
+
     }
 ];
 
