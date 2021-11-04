f20d9296da700bc93851e4768d4d6c9d584e2006
Keara Pereira <kearapereira@Kearas-MacBook-Pro.local>
2021-11-04 12:45:24 -0400

    seed data cleaning up

--- seedData.js
+++ seedData.js
@@ -1,27 +1,3 @@
-// /const movieSchema = new Schema(
-//     {
-//       image: {
-//         type: String,
-//       },
-//       overview: {
-//         type: String,
-//       },
-//       rate: {
-//         type: Number,
-//         required: [true, "Please rate the movie"],
-//         min: [0, "Must be 0 or greater"],
-//         max: [5, "5 or less"],
-//       },
-//       review: {
-//         type: String,
-//       },
-
-
-// const seedData = [{
-//     overview: "this is the movie",
-//     rate: 5,
-//     review: "its a cool movie"}
-// ]
 
 // Reviews
 const seedData = [
